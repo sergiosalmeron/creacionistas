eed0c31cec2be0dd6f9f642b34b8b14b14c6ffcc
netsergiosm <netsergiosm@a6ceee01-a62b-6199-61f7-7fbccab03a49>
2011-04-26 15:31:39 +0000

    Creado el OX con posiciones prioritarias

--- trunk/Practica1/src/Pruebas2.java
+++ trunk/Practica1/src/Pruebas2.java
@@ -3,6 +3,7 @@ import java.util.ArrayList;
 import com.sun.accessibility.internal.resources.accessibility;
 import com.sun.xml.internal.bind.v2.runtime.unmarshaller.XsiNilLoader.Array;
 
+import cruces.OXPosciciones;
 import cruces.RecombRutas;
 
 import logica.Cromosoma;
@@ -31,9 +32,14 @@ public class Pruebas2 {
 		}*/
 		
 		
-		/*
+		
 		Cromosoma a=new CromosomaCiudades();
 		Cromosoma b=new CromosomaCiudades();
+		System.out.print("Xxxxx:");
+		for (int i=0;i<b.getLongCromosoma();i++){
+			System.out.print(String.format("%3d", i)+",");
+		}
+		System.out.println();
 		System.out.print("Padre:");
 		for (int i=0;i<a.getLongCromosoma();i++){
 			System.out.print(String.format("%3d", a.getGen(i))+",");
@@ -45,28 +51,56 @@ public class Pruebas2 {
 		}
 		System.out.println();
 		
-		RecombRutas c=new RecombRutas();
+		OXPosciciones c=new OXPosciciones();
 		c.cruza(a, b);
 		
-		System.out.print("Hijo:");
+		System.out.print(" Hijo:");
 		for (int i=0;i<a.getLongCromosoma();i++){
 			System.out.print(String.format("%3d", a.getGen(i))+",");
 		}
 		System.out.println();
-		System.out.print("Hija:");
+		System.out.print(" Hija:");
 		for (int i=0;i<b.getLongCromosoma();i++){
 			System.out.print(String.format("%3d", b.getGen(i))+",");
 		}
 		System.out.println();
-		System.out.println("Tabla Adyacencias:");
-		c.muestraTabla();
+		c.muestraPosiciones();
 		
-		*/
+		ArrayList<Integer> aa=new ArrayList<Integer>();
+		ArrayList<Integer> bb=new ArrayList<Integer>();
+		for (int i=0;i<b.getLongCromosoma();i++){
+			aa.add(i+1);
+			bb.add(i+1);
+		}
 		
+		for (int i=0;i<b.getLongCromosoma();i++){
+			if (aa.contains(a.getGen(i))){
+				aa.remove((Object)a.getGen(i));
+			}
+			else
+				System.err.println("elemento de a repetido: "+a.getGen(i));
+			
+			
+			if (bb.contains(b.getGen(i))){
+				bb.remove((Object)b.getGen(i));
+			}
+			else
+				System.err.println("elemento de b repetido: "+b.getGen(i));
+		}
+		
+		//c.muestraTabla();
+		
+		
+		/*
 		Cromosoma a=new CromosomaCiudades();
-		Heuristica b=new Heuristica();
-		b.muta(a);
+		Cromosoma b=new CromosomaCiudades();
+		OXPosciciones c=new OXPosciciones();
+		c.cruza(a, b);*/
+		//Heuristica b=new Heuristica();
+	//	b.muta(a);
 		
 	}
+	
+	
 
 }
