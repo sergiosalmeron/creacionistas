dae89f19e29983f8ce7453c68ebe3232a32d0d08
netsergiosm <netsergiosm@a6ceee01-a62b-6199-61f7-7fbccab03a49>
2011-05-10 15:30:20 +0000

    arbol y tipos

---
+++ trunk/Practica3/src/pruebArbol.java
@@ -0,0 +1,32 @@
+import logica.Arbol;
+
+
+public class pruebArbol {
+	public static void main (String[] args){
+		Arbol a=Arbol.getRandArbol(0);
+		Arbol b=Arbol.getRandArbol(0);
+		System.out.println(a);
+		System.out.println("--------");
+		System.out.println(b);
+		System.out.println("--------");
+		ejecuta(a,b);
+		System.out.println(a);
+		System.out.println("--------");
+		System.out.println(b);
+		System.out.println("--------");
+	}
+	
+	private static void ejecuta(Arbol crom1, Arbol crom2) {
+		Arbol a = crom1.getRandSubArbol();
+		Arbol b = crom2.getRandSubArbol();
+		System.out.println(a);
+		System.out.println("--------");
+		System.out.println(b);
+		System.out.println("--------");
+
+		Arbol.intercambiarArboles(a, b);
+
+	}
+
+
+}
